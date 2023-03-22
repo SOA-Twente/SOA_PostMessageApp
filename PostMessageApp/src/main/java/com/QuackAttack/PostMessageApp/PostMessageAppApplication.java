20c34f01b0a7ecec2f4cea5cb44d727848bbfa3f
Yun <81588081+YunFeng-Zheng@users.noreply.github.com>
2023-03-22 22:05:34 +0100

    Add files via upload

---
+++ PostMessageApp/src/main/java/com/QuackAttack/PostMessageApp/PostMessageAppApplication.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.PostMessageApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostMessageAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostMessageAppApplication.class, args);
+	}
+
+}
---
+++ PostMessageApp/src/test/java/com/QuackAttack/PostMessageApp/PostMessageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuackAttack.PostMessageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostMessageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
