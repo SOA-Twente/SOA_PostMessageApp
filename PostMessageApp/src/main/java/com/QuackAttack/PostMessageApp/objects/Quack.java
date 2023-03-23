08a203ea2ec09ffb7490649bdd77d48e03af2de6
Yun Feng Zheng <y.f.zheng@student.utwente.nl>
2023-03-23 12:56:57 +0100

    Added post method for quacks

--- PostMessageApp/src/main/java/com/QuackAttack/PostMessageApp/objects/Quack.java
+++ PostMessageApp/src/main/java/com/QuackAttack/PostMessageApp/objects/Quack.java
@@ -20,4 +20,71 @@ public class Quack {
         this.retweetedQuackId = retweetedQuackId;
         this.createdAt = createdAt;
     }
+
+    public Quack() {
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public boolean isReply() {
+        return isReply;
+    }
+
+    public void setReply(boolean reply) {
+        isReply = reply;
+    }
+
+    public int getRepliedQuackId() {
+        return repliedQuackId;
+    }
+
+    public void setRepliedQuackId(int repliedQuackId) {
+        this.repliedQuackId = repliedQuackId;
+    }
+
+    public boolean isRetweet() {
+        return isRetweet;
+    }
+
+    public void setRetweet(boolean retweet) {
+        isRetweet = retweet;
+    }
+
+    public int getRetweetedQuackId() {
+        return retweetedQuackId;
+    }
+
+    public void setRetweetedQuackId(int retweetedQuackId) {
+        this.retweetedQuackId = retweetedQuackId;
+    }
 }
--- PostMessageApp/src/main/java/com/QuackAttack/PostMessageApp/web/IndexController.java
+++ PostMessageApp/src/main/java/com/QuackAttack/PostMessageApp/web/IndexController.java
@@ -1,9 +1,32 @@
 package com.QuackAttack.PostMessageApp.web;
 
 
+import com.QuackAttack.PostMessageApp.objects.Quack;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.jdbc.core.JdbcTemplate;
+
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @PostMapping("/postQuack")
+    public Quack postMessage(@RequestBody Quack message){
+        String sql = "INSERT INTO quacks (user_id, message, is_reply, reply_to_quack_id, is_retweet, retweet_to_quack_id) VALUES (?,?, ?, ?, ?, ?)";
+
+        int rows = jdbcTemplate.update(sql, message.getUserId(), message.getMessage(), message.isReply(), message.getRepliedQuackId(), message.isRetweet(), message.getRetweetedQuackId());
+        if (rows > 0) {
+            //If row has been created
+            System.out.println("A new row has been inserted.");
+        }
+        else {
+            //If row has not been created
+            System.out.println("Something went wrong.");
+        }
+        return message;
+    }
 }
